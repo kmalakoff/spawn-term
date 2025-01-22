18a1a8547d18af4d822ba3c4bbcc07d40c918ad2
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-21 22:58:59 -0800

    Upgrade modules

--- src/lib/concatWritable.ts
+++ src/lib/concatWritable.ts
@@ -1,14 +1,14 @@
+import eos from 'end-of-stream';
 import { Writable } from 'readable-stream';
 
 export default function concatWritable(callback) {
   const chunks = [];
-
   const stream = new Writable({
     write: (chunk, _encoding, next) => {
       chunks.push(chunk);
       next();
     },
   });
-  stream.on('finish', () => callback(Buffer.concat(chunks.splice(0))));
+  eos(stream, () => callback(Buffer.concat(chunks.splice(0))));
   return stream;
 }
