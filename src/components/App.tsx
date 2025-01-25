117dce4d063381aa1869e2dad7e5858187abf1d1
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-25 13:31:20 -0800

    Simplify process rendering in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,22 +8,12 @@ import ChildProcess from './ChildProcess';
 export default function App() {
   const store = useContext(StoreContext);
   const appState = useStore(store) as AppState;
-  const running = appState.processes.filter((x) => ['running'].indexOf(x.state) >= 0);
-  const done = appState.processes.filter((x) => ['error', 'success'].indexOf(x.state) >= 0);
 
   return (
-    <>
-      <Box flexDirection="column">
-        {running.map((item: ChildProcessT) => (
-          <ChildProcess key={item.id} id={item.id} />
-        ))}
-      </Box>
-      <Box flexDirection="column" borderStyle="single" borderColor="black" borderBottom={true} borderTop={false} borderLeft={false} borderRight={false} />
-      <Box flexDirection="column">
-        {done.map((item: ChildProcessT) => (
-          <ChildProcess key={item.id} id={item.id} />
-        ))}
-      </Box>
-    </>
+    <Box flexDirection="column">
+      {appState.processes.map((item: ChildProcessT) => (
+        <ChildProcess key={item.id} id={item.id} />
+      ))}
+    </Box>
   );
 }
