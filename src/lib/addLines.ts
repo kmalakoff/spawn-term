a527cfe3300d4269c9ae467b99de6976bb535409
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-28 23:08:20 -0800

    Refactor addLines to use newline-iterator for line splitting

--- src/lib/addLines.ts
+++ src/lib/addLines.ts
@@ -1,21 +1,28 @@
+import newlineIterator from 'newline-iterator';
 import { Writable } from 'readable-stream';
-
-const REGEX_NEW_LINE = /\r?\n|\r/g;
 
 export default function addLines(fn) {
   let last = '';
 
   const stream = new Writable({
     write(chunk, _enc, callback) {
-      const more = last + chunk.toString('utf8');
-      const lines = more.split(REGEX_NEW_LINE);
+      const string = last + chunk.toString('utf8');
+
+      const lines = [];
+      const iterator = newlineIterator(string);
+      let next = iterator.next();
+      while (!next.done) {
+        lines.push(next.value);
+        next = iterator.next();
+      }
+
       last = lines.pop();
       if (lines.length > 0) fn(lines);
       callback();
     },
   });
   stream.on('finish', () => {
-    if (last.length) fn(last);
+    if (last.length) fn([last]);
     last = '';
   });
   return stream;
