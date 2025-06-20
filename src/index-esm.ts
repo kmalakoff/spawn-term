5ba5fc5fed16627d90a456dd3feb3b9856434a56
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 17:47:52 -0700

    Upgrade modules

--- src/index-esm.ts
+++ src/index-esm.ts
@@ -1,31 +1,8 @@
-import type { SpawnOptions, SpawnResult, TerminalCallback, TerminalOptions } from './types.js';
-
-function worker(command: string, args: string[], spawnOptions: SpawnOptions, options: TerminalOptions, callback: TerminalCallback): undefined {
-  import('./worker.js')
-    .then((fn) => {
-      fn.default(command, args, spawnOptions, options, callback);
-    })
-    .catch(callback);
-}
-
-function spawnTerminal(command: string, args: string[], spawnOptions: SpawnOptions, options?: TerminalOptions | TerminalCallback, callback?: TerminalCallback): undefined | Promise<SpawnResult> {
-  if (typeof options === 'function') {
-    callback = options as TerminalCallback;
-    options = {};
-  }
-  options = options || {};
-
-  if (typeof callback === 'function') return worker(command, args, spawnOptions, options, callback as TerminalCallback);
-  return new Promise((resolve, reject) =>
-    worker(command, args, spawnOptions, options, (err, result) => {
-      err ? reject(err) : resolve(result);
-    })
-  );
-}
-
-const major = +process.versions.node.split('.')[0];
-
 export { default as figures } from './lib/figures.js';
 export { default as formatArguments } from './lib/formatArguments.js';
 export * from './types.js';
-export default major > 14 ? spawnTerminal : undefined;
+
+const major = +process.versions.node.split('.')[0];
+
+import { default as spawnTerminal } from './spawnTerminal.js';
+export default major > 14 ? spawnTerminal : (undefined as typeof spawnTerminal);
