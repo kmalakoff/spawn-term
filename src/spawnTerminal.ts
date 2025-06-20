46fd51893a82d113a26c8349ba19045f9f2e2ed4
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 17:47:52 -0700

    Upgrade modules

---
+++ src/spawnTerminal.ts
@@ -0,0 +1,24 @@
+import type { SpawnOptions, SpawnResult, TerminalCallback, TerminalOptions } from './types.js';
+
+function worker(command: string, args: string[], spawnOptions: SpawnOptions, options: TerminalOptions, callback: TerminalCallback): undefined {
+  import('./worker.js')
+    .then((fn) => {
+      fn.default(command, args, spawnOptions, options, callback);
+    })
+    .catch(callback);
+}
+
+export default function spawnTerminal(command: string, args: string[], spawnOptions: SpawnOptions, options?: TerminalOptions | TerminalCallback, callback?: TerminalCallback): undefined | Promise<SpawnResult> {
+  if (typeof options === 'function') {
+    callback = options as TerminalCallback;
+    options = {};
+  }
+  options = options || {};
+
+  if (typeof callback === 'function') return worker(command, args, spawnOptions, options, callback as TerminalCallback);
+  return new Promise((resolve, reject) =>
+    worker(command, args, spawnOptions, options, (err, result) => {
+      err ? reject(err) : resolve(result);
+    })
+  );
+}
