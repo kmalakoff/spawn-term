7eac8668089b4e4d352d2933399b7605a0dce3ae
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-20 06:38:03 -0700

    Throttle process store updates and remove ProcessProvider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,11 @@
 import { Box } from 'ink';
-import { useProcesses } from '../state/ProcessContext.js';
 import type { ChildProcess as ChildProcessT } from '../types.js';
 import ChildProcess from './ChildProcess.js';
 
-export default function App() {
-  const processes = useProcesses();
-
+export default function App({ store }) {
   return (
     <Box flexDirection="column">
-      {processes.map((item: ChildProcessT) => (
+      {store.processes.map((item: ChildProcessT) => (
         <ChildProcess key={item.id} {...item} />
       ))}
     </Box>
