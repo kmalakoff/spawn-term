1a2642b1aa815744abc74cb15a558fe556b3b8f2
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 17:47:52 -0700

    Extract spawnTerminal logic into its own module

--- src/index-cjs.ts
+++ src/index-cjs.ts
@@ -1,4 +1,6 @@
 export { default as figures } from './lib/figures.js';
 export { default as formatArguments } from './lib/formatArguments.js';
 export * from './types.js';
-export default undefined;
+
+import type { default as spawnTerminal } from './spawnTerminal.js';
+export default undefined as typeof spawnTerminal;
