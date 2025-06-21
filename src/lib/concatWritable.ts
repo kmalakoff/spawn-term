756475272bc0a00db4d39e84d7f8ed6ebb6ab33e
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 09:19:32 -0700

    Add types to App component and update concatWritable return type

--- src/lib/concatWritable.ts
+++ src/lib/concatWritable.ts
@@ -1,8 +1,10 @@
-import { Writable } from 'stream';
+import Stream from 'stream';
+
+const Writable = Stream.Writable;
 
 export type Callback = (lines: Buffer) => undefined;
 
-export default function concatWritable(callback: Callback): Writable {
+export default function concatWritable(callback: Callback): NodeJS.WritableStream {
   const chunks = [];
   const stream = new Writable({
     write: (chunk, _encoding, next) => {
