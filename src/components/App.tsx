93365920454878e5ce23ecfbb7b57e5a4c1f46fc
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 09:19:32 -0700

    Add types to App component and update concatWritable return type

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import { Box } from 'ink';
+import type Store from '../state/Store.ts';
 import type { ChildProcess as ChildProcessT } from '../types.ts';
 import ChildProcess from './ChildProcess.ts';
 
-export default function App({ store }) {
+export interface AppProps {
+  store: Store;
+}
+
+export default function App({ store }: AppProps): React.JSX.Element {
   return (
     <Box flexDirection="column">
       {store.processes.map((item: ChildProcessT) => (
