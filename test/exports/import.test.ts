cf61d59301c4bb3345bef3538d172992226f50f0
Kevin Malakoff <kmalakoff@gmail.com>
2025-11-28 14:33:10 -0800

    Added tests for mutually exclusive conditions to match cross-spawn-cb

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -7,12 +7,12 @@ const major = +process.versions.node.split('.')[0];
 
 describe('exports .js', () => {
   it('named exports', () => {
-    assert.equal(typeof spawnTerminal, major > 14 ? 'function' : 'undefined');
+    assert.equal(typeof spawnTerminal, major > 18 ? 'function' : 'undefined');
     assert.equal(typeof figures, 'object');
   });
 
   it('default exports', () => {
-    assert.equal(typeof spawnTerminalStar.default, major > 14 ? 'function' : 'undefined');
+    assert.equal(typeof spawnTerminalStar.default, major > 18 ? 'function' : 'undefined');
     assert.equal(typeof spawnTerminalStar.figures, 'object');
   });
 });
